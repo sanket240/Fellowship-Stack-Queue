bb0ecafdbefd02abbde85bcbacc2bb4fddf13f6f
sanket2498 <sanketdulange24@gmail.com>
2021-01-04 00:13:39 +0530

    Added : Create Stack three elements

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,19 @@
+public class MyStack<K> {
+    public final MyLinkedList<K> myLinkedList;
+    public MyStack()
+    {
+        myLinkedList=new MyLinkedList<>();
+    }
+    public void push(INode<K> element)
+    {
+        myLinkedList.add(element);
+    }
+    public INode<K> peak()
+    {
+        return myLinkedList.head;
+    }
+    public void printStack()
+    {
+        myLinkedList.printMyNodes();
+    }
+}
---
+++ src/test/java/MyStackTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyStackTest {
+    @Test
+    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
+        MyStack<Integer> myStack=new MyStack<>();
+        MyNode<Integer> myFirstNode =new MyNode<>(56);
+        MyNode<Integer> mySecondNode =new MyNode<>(30);
+        MyNode<Integer> myThirdNode =new MyNode<>(70);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode<Integer> myNode=myStack.peak();
+        myStack.printStack();
+        Assert.assertEquals(myThirdNode,myNode);
+    }
+}
